3515518ca9130484868353974d1de6e9b8e0929b
James Adney <jfadney@gmail.com>
2013-05-22 17:17:23 -0700

    rails app with basic gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grades::Application.config.secret_token = 'e0eed4bac7a33be5d00776c41fab69d45b7ea9b5c63932583500f0d9c3f2b7639e29ebca3ea8c280e88d333b8f9966850444efde0c70e6cc53e7a582f8fdb345'
