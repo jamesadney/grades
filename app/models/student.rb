1eade345cf1e20b98e5272ff974b2b4fe434523d
James Adney <jfadney@gmail.com>
2013-06-02 13:58:50 -0700

    show students that already have grade on assignment page

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,14 @@ class Student < ActiveRecord::Base
 
   has_many :grades
   has_many :assignments, through: :grades
+
+  def self.has_score_for(assignment)
+    # TODO: Implicit or explicit self?
+    joins(:grades).where("grades.assignment_id" => assignment.id)
+  end
+
+  def grade_for(assignment)
+    # FIXME: Why can't I do find_by_assignment(assignment)?
+    self.grades.find_by_assignment_id(assignment.id)
+  end
 end
